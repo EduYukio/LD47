79ded54dc28b29ce9cfc75a05abca202ef18e192
Eduardo Yukio <eduardoyrodrigues@gmail.com>
2020-10-03 02:48:40 -0300

    Add player script, walk and jump mechanic

---
+++ Assets/Scripts/GroundCheck.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GroundCheck : MonoBehaviour {
+    Player playerScript;
+
+    void Start() {
+        GameObject playerObject = transform.parent.gameObject;
+        playerScript = playerObject.GetComponent<Player>();
+    }
+
+    void Update() {
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Ground") {
+            playerScript.isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other) {
+        if (other.gameObject.tag == "Ground") {
+            playerScript.isGrounded = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+    public float moveSpeed = 5f;
+    public float jumpForce = 5f;
+    public bool isGrounded = false;
+    Rigidbody2D rb;
+
+    void Start() {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() {
+        float xInput = Input.GetAxisRaw("Horizontal");
+        if (xInput != 0) {
+            Walk(xInput);
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded) {
+            Jump();
+        }
+    }
+
+    void Walk(float xInput) {
+        Vector3 direction = new Vector3(xInput, 0f, 0f);
+        transform.position += direction * Time.deltaTime * moveSpeed;
+    }
+
+    void Jump() {
+        rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+    }
+}
